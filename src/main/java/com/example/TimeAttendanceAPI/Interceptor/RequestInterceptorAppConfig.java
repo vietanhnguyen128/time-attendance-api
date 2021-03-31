d688500318d219dd784e5fab083e3c5cfac08bc4
Nguyen Viet Anh <anhnv621@wru.vn>
2021-03-31 21:24:59 +0700

    bla bla

--- src/main/java/com/example/TimeAttendanceAPI/Interceptor/RequestInterceptorAppConfig.java
+++ src/main/java/com/example/TimeAttendanceAPI/Interceptor/RequestInterceptorAppConfig.java
@@ -1,20 +1,20 @@
-package com.example.TimeAttendanceAPI.Interceptor;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Component
-public class RequestInterceptorAppConfig implements WebMvcConfigurer {
-
-    @Autowired
-    RequestInterceptor requestInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(requestInterceptor)
-                .excludePathPatterns("/login")
-                .excludePathPatterns("/docs/**");
-    }
-}
+//package com.example.TimeAttendanceAPI.Interceptor;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Component
+//public class RequestInterceptorAppConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    RequestInterceptor requestInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(requestInterceptor)
+//                .excludePathPatterns("/login")
+//                .excludePathPatterns("/docs/**");
+//    }
+//}
