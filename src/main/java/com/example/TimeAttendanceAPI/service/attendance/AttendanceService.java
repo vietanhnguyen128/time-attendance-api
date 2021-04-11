8eb19bcef7f00c782270bb4649b6556a0335516b
Nguyen Viet Anh <anhnv621@wru.vn>
2021-04-11 21:46:13 +0700

    add services

--- src/main/java/com/example/TimeAttendanceAPI/service/attendance/AttendanceService.java
+++ src/main/java/com/example/TimeAttendanceAPI/service/attendance/AttendanceService.java
@@ -1,4 +1,17 @@
 package com.example.TimeAttendanceAPI.service.attendance;
 
+import com.example.TimeAttendanceAPI.dto.AttendanceRecordDTO;
+
+import java.util.List;
+
 public interface AttendanceService {
+    AttendanceRecordDTO createAttendanceRecord(AttendanceRecordDTO AttendanceRecordDTO);
+
+    List<AttendanceRecordDTO> getAttendanceRecordList();
+
+    AttendanceRecordDTO getSingleAttendanceRecord(String AttendanceRecordId);
+
+    AttendanceRecordDTO updateAttendanceRecord(AttendanceRecordDTO AttendanceRecordDTO);
+
+    void deleteAttendanceRecord(String AttendanceRecordId);
 }
--- src/main/java/com/example/TimeAttendanceAPI/service/department/DepartmentService.java
+++ src/main/java/com/example/TimeAttendanceAPI/service/department/DepartmentService.java
@@ -1,4 +1,16 @@
 package com.example.TimeAttendanceAPI.service.department;
+import com.example.TimeAttendanceAPI.dto.DepartmentDTO;
+
+import java.util.List;
 
 public interface DepartmentService {
+    DepartmentDTO createDepartment(DepartmentDTO departmentDTO);
+
+    List<DepartmentDTO> getDepartmentList();
+
+    DepartmentDTO getSingleDepartment(String departmentId);
+
+    DepartmentDTO updateDepartment(DepartmentDTO departmentDTO);
+
+    void deleteDepartment(String departmentId);
 }
