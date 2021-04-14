dc8409f8b174784a84ed8be7aa6badd042ae7320
Nguyen Viet Anh <anhnv02@ominext.com>
2021-04-14 14:27:57 +0700

    refactor

--- src/main/java/com/example/TimeAttendanceAPI/service/attendance/AttendanceService.java
+++ src/main/java/com/example/TimeAttendanceAPI/service/attendance/AttendanceService.java
@@ -2,6 +2,7 @@ package com.example.TimeAttendanceAPI.service.attendance;
 
 import com.example.TimeAttendanceAPI.dto.AttendanceRecordDTO;
 
+import java.time.Duration;
 import java.util.List;
 
 public interface AttendanceService {
@@ -14,4 +15,26 @@ public interface AttendanceService {
     AttendanceRecordDTO updateAttendanceRecord(AttendanceRecordDTO AttendanceRecordDTO);
 
     void deleteAttendanceRecord(String AttendanceRecordId);
+
+    Duration getAttendanceTimeByDay(Integer employeeId, String date);
+
+    Duration getAttendanceTimeByPeriod(Integer employeeId, String startDate, String endDate);
+
+    Duration getTotalAttendanceTime(Integer employeeId);
+
+    Duration getTotalLateTime(Integer employeeId);
+
+    Duration getLateTimeByPeriod(Integer employeeId, String startDate, String endDate);
+
+    Duration getTotalWorkingTime(Integer employeeId);
+
+    Duration getWorkingTimeByPeriod(Integer employeeId, String startDate, String endDate);
+
+    Duration getTotalAbsentTime(Integer employeeId);
+
+    Duration getAbsentTimeByPeriod(Integer employeeId, String startDate, String endDate);
+
+    Duration getTotalOvertime(Integer employeeId);
+
+    Duration getOvertimeByPeriod(Integer employeeId, String startDate, String endDate);
 }
--- src/main/java/com/example/TimeAttendanceAPI/service/general/GeneralService.java
+++ src/main/java/com/example/TimeAttendanceAPI/service/general/GeneralService.java
@@ -7,29 +7,5 @@ import java.time.Duration;
 
 public interface GeneralService {
 
-    AttendanceRecord createAttendanceRecord(AttendanceRecord record);
 
-    FormRecord createForm(FormRecord form);
-
-    Duration getAttendanceTimeByDay(Integer employeeId, String date);
-
-    Duration getAttendanceTimeByPeriod(Integer employeeId, String startDate, String endDate);
-
-    Duration getTotalAttendanceTime(Integer employeeId);
-
-    Duration getTotalLateTime(Integer employeeId);
-
-    Duration getLateTimeByPeriod(Integer employeeId, String startDate, String endDate);
-
-    Duration getTotalWorkingTime(Integer employeeId);
-
-    Duration getWorkingTimeByPeriod(Integer employeeId, String startDate, String endDate);
-
-    Duration getTotalAbsentTime(Integer employeeId);
-
-    Duration getAbsentTimeByPeriod(Integer employeeId, String startDate, String endDate);
-
-    Duration getTotalOvertime(Integer employeeId);
-
-    Duration getOvertimeByPeriod(Integer employeeId, String startDate, String endDate);
 }
