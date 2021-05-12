349b5b4f0dd07a6a6114a72687984ffc1823d3e7
Nguyen Viet Anh <anhnv621@wru.vn>
2021-05-12 22:17:16 +0700

    get attendance by year and month

--- src/main/java/com/example/TimeAttendanceAPI/service/attendance/AttendanceServiceImpl.java
+++ src/main/java/com/example/TimeAttendanceAPI/service/attendance/AttendanceServiceImpl.java
@@ -9,25 +9,27 @@ import com.example.TimeAttendanceAPI.repository.AttendanceCacheRepository;
 import com.example.TimeAttendanceAPI.repository.AttendanceRepository;
 import com.example.TimeAttendanceAPI.repository.UserRepository;
 import com.example.TimeAttendanceAPI.security.service.CustomUserDetails;
+import com.example.TimeAttendanceAPI.service.form_record.FormRecordService;
 import com.example.TimeAttendanceAPI.utils.PageableUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.jni.Local;
+import org.apache.commons.collections4.ListUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,6 +39,7 @@ public class AttendanceServiceImpl implements AttendanceService {
     private final UserRepository userRepository;
     private final AttendanceRepository attendanceRepository;
     private final AttendanceCacheRepository attendanceCacheRepository;
+    private final FormRecordService formRecordService;
 
     @Override
     public Page<AttendanceRecordDTO> getAttendanceRecordList(int pageNo, int pageSize, String sortBy) {
@@ -97,12 +100,34 @@ public class AttendanceServiceImpl implements AttendanceService {
 
     @Override
     public AttendanceInfo getAttendanceInfo(int userId, int month, int year) {
-        return null;
+        AttendanceInfo result = new AttendanceInfo();
+        long totalAttendanceInMinutes = 0;
+
+        DateOfMonth parsed = constructLocalDate(month, year);
+        List<AttendanceRecord> attendanceRecordOfMonth = attendanceRepository
+                .findALlByUser_UserIdAndDateBetween(userId, parsed.getFromDate(), parsed.getToDate());
+
+        List<AttendanceRecord> checkInRecords = attendanceRecordOfMonth.stream().filter(AttendanceRecord::isCheckIn).collect(Collectors.toList());
+        List<AttendanceRecord> checkOutRecords = ListUtils.subtract(attendanceRecordOfMonth, checkInRecords);
+
+        Queue<AttendanceRecord> checkInQueue = new LinkedList<>(checkInRecords);
+        Queue<AttendanceRecord> checkOutQueue = new LinkedList<>(checkOutRecords);
+
+        while (!checkInQueue.isEmpty()) {
+            AttendanceRecord checkIn = checkInQueue.poll();
+            AttendanceRecord checkOut = checkOutQueue.peek();
+
+            if (checkIn.getDate().getDayOfMonth() == checkOut.getDate().getDayOfMonth()) {
+                totalAttendanceInMinutes+= getDifference(checkIn.getTimestamp(), checkOut.getTimestamp());
+            }
+        }
+
+        return result;
     }
 
     @Override
     public List<AttendanceRecordDTO> getAttendanceRecords(int userId, int month, int year) {
-        dateOfMonth parsed = constructLocalDate(month, year);
+        DateOfMonth parsed = constructLocalDate(month, year);
         List<AttendanceRecord> result = attendanceRepository.findALlByUser_UserIdAndDateBetween(
                 userId,
                 parsed.getFromDate(),
@@ -115,7 +140,7 @@ public class AttendanceServiceImpl implements AttendanceService {
         return (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
-    private dateOfMonth constructLocalDate(int month, int year) {
+    private DateOfMonth constructLocalDate(int month, int year) {
         String startDate = "01/";
         String endDate;
 
@@ -134,19 +159,35 @@ public class AttendanceServiceImpl implements AttendanceService {
             endDate = endOfMonth + startDate.substring(2);
         }
 
-        return new dateOfMonth(startDate, endDate);
+        return new DateOfMonth(startDate, endDate);
+    }
+
+    private long getDifference(LocalTime checkIn, LocalTime checkOut) {
+        long elapsedMinutes = Duration.between(checkOut, checkIn).toMinutes();
+
+        long subtractBreakTime;
+
+        if (checkOut.compareTo(LocalTime.of(12, 0)) <= 0) {
+            subtractBreakTime = 0;
+        } else if (checkOut.compareTo(LocalTime.of(13, 0)) >= 0) {
+            subtractBreakTime = 60;
+        } else {
+            subtractBreakTime = Duration.between(checkOut, LocalTime.of(12, 0)).toMinutes();
+        }
+
+        return elapsedMinutes - subtractBreakTime;
     }
 }
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-class dateOfMonth {
+class DateOfMonth {
     private LocalDate fromDate;
 
     private LocalDate toDate;
 
-    public dateOfMonth(String startDate, String endDate) {
+    public DateOfMonth(String startDate, String endDate) {
         fromDate = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         toDate = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
