dd09bdbcedc7de520c453b392148ba51caedf935
Nguyen Viet Anh <anhnv621@wru.vn>
2021-05-24 21:45:53 +0700

    add exception handler

---
+++ src/main/java/com/example/TimeAttendanceAPI/service/_exception/ApiExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.TimeAttendanceAPI.service._exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleAllException(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
